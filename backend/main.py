4a8aae88f4ec5d9046e9365ee20fcd6f417e427a
shubham S <sharmashubham8412@gmail.com>
2026-05-18 11:18:24 +0530

    added put method

--- backend/main.py
+++ backend/main.py
@@ -32,29 +32,24 @@ def home(): # if request comes to "/" endpoint, this function will be executed a
 #api to fetch student report card based on student id
 @app.get("/students/{student_id}") # define endpoint with path parameter student_id
 def get_student(student_id: int):
+
+    #check is student_id is present or not
+    if student_id not in students:
+        return {"error": "Student not found"} # return error message if student not found
+
     # fetch student data from dictonary using student_id as key
-    student = students.get(student_id)
+    student=students[student_id]
 
     #check if student exists
     if not student:
         return {"error": "Student not found"} # return error message if student not found
-    
-    #calculate total marks
-    total_marks = (
-        student["maths"] + student["science"] + student["english"]
-    )
-
-    #claculate percentage
-    percentage = total_marks / 3
 
     #return student report card as JSON response
     return {
         "name": student["name"],
         "maths": student["maths"],
         "science": student["science"],
-        "english": student["english"],
-        "total_marks": total_marks,
-        "percentage": percentage
+        "english": student["english"]
     }
 
 
@@ -70,6 +65,11 @@ class Student(BaseModel): #BaseModel is a class from pydantic module that we can
 #Creating post API to add new student record
 @app.post("/student")
 def add_student(student:Student): #receive request body conevrt into student object and validate automatically.
+
+    #check if student exists or not
+    if student.id in students:
+        return {"error": "Student with this id already exists"}
+    
     #add student into dictonary
     students[student.id]={
         "name": student.name,
@@ -78,3 +78,51 @@ def add_student(student:Student): #receive request body conevrt into student obj
         "english": student.english
     }
     return {"message": f"student {student.name} added successfully!"}
+
+#Now generating student report
+@app.get("/students/{student_id}/report")
+def get_student_report(student_id:int):
+
+    #check is student_id is present or not
+    if student_id not in students:
+        return {"error": "Student not found"} # return error message if student not found
+
+    # fetch student data from dictonary using student_id as key
+    student=students[student_id]
+
+    #find total marks, percentage and grade
+    total_marks=(student["maths"]+student["science"]+student["english"])
+    percentage=total_marks/3
+
+    if percentage>90:
+        grade="A"
+    elif percentage>75:
+        grade="B"
+    elif percentage>50:
+        grade="C"
+    else:
+        grade="Fail"
+
+    return {
+        "name": student["name"],
+        "total_marks": total_marks,
+        "percentage": percentage,
+        "grade": grade
+        }
+
+#Now updating student record
+@app.put("/student/{student_id}")
+def update_student_record(student_id:int, student:Student):
+    #check the id which we want to update is present or not
+    if student_id not in students:
+        return {"error": "Student not found"} # return error message if student not found
+    
+    #update student record
+    students[student_id]={
+        "name": student.name,
+        "maths": student.maths,
+        "science": student.science, 
+        "english": student.english
+    }
+    return {"message": f"student {student_id} updated successfully!"}
+
